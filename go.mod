43b3b00a418898a5b4824edd24a93b7083b4f605
Tymofii Kliuiev <pungentee@gmail.com>
2023-12-23 05:16:27 +0200

    full grammar check is implemented

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module calculator
 
 go 1.21.5
+
+require github.com/Pungentee/stack v1.0.0 // indirect
--- src/grammarChecking.go
+++ src/grammarChecking.go
@@ -3,61 +3,43 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/Pungentee/stack"
 	"regexp"
+	"strings"
 )
 
 /*
 	Grammatical rules
 
-number:
-	1. can be single
-	2. to left of it can place an action or the opening bracket
-	3. to right of it can place an action or the closing bracket
-
 plus, minis, star, slash (actions):
 	1. can not be single
-	2. to left of it must place a number or the closing bracket
-	3. to right of it must place a number or the opening bracket
+	2. to left of it must be placed a number or the closing bracket
+	3. to right of it must be placed a number or the opening bracket
 
 both type of brackets:
 	1. a pair is required
 	2. if a pair is immediately after the opening bracket: error
+	3. brackets of the same type can be placed next to brackets
 
 opening bracket:
-	1. to left of it can place an action or an opening bracket
-	2. to right of it can place a number
+	1. to left of it must place an action
+	2. to right of it must place a number
 
 closing bracket:
-	1. to left of it can place a number or a closing bracket
-	2. to right of it can place an action
+	1. to left of it must place a number (not implemented because it is processed from rule 3 for actions)
+	2. to right of it must place an action
 */
 
 func grammarCheck(tokens []string) error {
+	var bracketsStack stack.Stack[int] // contains an index of pairs
 	numRegEx, err := regexp.Compile("[+-]?[0-9]+[.]?[0-9]*([e][+-]?[0-9]+)?")
 	if err != nil {
 		return err
 	}
 
 	for index, value := range tokens {
-		// Fro numbers:
-		if numRegEx.MatchString(value) {
-			// rule #2
-			if index != 0 {
-				prevToken := tokens[index-1]
-				if prevToken != "+" && prevToken != "-" && prevToken != "*" && prevToken != "/" && prevToken != "(" {
-					return errors.New(fmt.Sprintf("at %d: unexpected \"%s\"", index, prevToken))
-				}
-			}
-			// rule #3
-			if index != len(tokens)-1 {
-				nextToken := tokens[index+1]
-				if nextToken != "+" && nextToken != "-" && nextToken != "*" && nextToken != "/" && nextToken != ")" {
-					return errors.New(fmt.Sprintf("at %d: unexpected \"%s\"", index+2, nextToken))
-				}
-			}
-		} else if value == "+" || value == "-" || value == "*" || value == "/" {
+		if value == "+" || value == "-" || value == "*" || value == "/" {
 			// For actions
-
 			// rule #1
 			if len(tokens) == 1 {
 				return errors.New("action operator can not be single")
@@ -75,9 +57,57 @@ func grammarCheck(tokens []string) error {
 					return errors.New(fmt.Sprintf("at %d: unexpected right operand: \"%s\", expects a number or the opening bracket", index+1, nextToken))
 				}
 			}
-		} else if value == "(" || value == ")" {
-			// For both type of brackets
+		} else if value == "(" {
+			// For both type of brackets: rule #1
+			// if found an opening bracket adding it to bracket stack
+			bracketsStack.Push(index)
+
+			// For opening brackets and rule #3 for both type of brackets
+			// if current token is not first then check rule #1
+			if index != 0 {
+				if prevToken := tokens[index-1]; prevToken != "+" && prevToken != "-" && prevToken != "*" && prevToken != "/" && prevToken != "(" {
+					return errors.New(fmt.Sprintf("at %d: unexpected \"%s\"; expects an action or the openting bracket", index, prevToken))
+				}
+			}
+			// if current token is not last then check rule #2
+			if index != len(tokens)-1 {
+				if nextToken := tokens[index+1]; !numRegEx.MatchString(nextToken) && nextToken != "(" {
+					if nextToken == ")" {
+						return errors.New(fmt.Sprintf("at %d: empty pairs of brackets are not allowed", index))
+					}
+					return errors.New(fmt.Sprintf("at %d: unexpected \"%s\"; expects a number or the openting bracket", index+2, nextToken))
+				}
+			}
+		} else if value == ")" {
+			// For both type of brackets: rule #1
+			// if found a pair of opening bracket removing its from stack
+			openBracketIndex, err := bracketsStack.Pop()
+			if err != nil {
+				return errors.New(fmt.Sprintf("at %d: not found a pair of \")\"", index+1))
+			}
+
+			// For both type of brackets: rule #2
+			if openBracketIndex+1 == index {
+				return errors.New(fmt.Sprintf("at %d: empty pairs of brackets are not allowed", index))
+			}
+
+			// For closing brackets
+			// if current token is not last then check rule #2
+			if index != len(tokens)-1 {
+				if nextToken := tokens[index+1]; nextToken != "+" && nextToken != "-" && nextToken != "*" && nextToken != "/" && nextToken != ")" {
+					return errors.New(fmt.Sprintf("at %d: unexpected \"%s\"; expects an action or the closing brackets", index+2, nextToken))
+				}
+			}
 		}
+	}
+	// For both type of brackets: rule #1
+	// if the stack is not empty, not all pairs are found
+	if len(bracketsStack) != 0 {
+		var errMsg []string
+		for _, index := range bracketsStack {
+			errMsg = append(errMsg, fmt.Sprintf("at %d: not found a pair of \"(\"", index+1))
+		}
+		return errors.New(strings.Join(errMsg, "\n"))
 	}
 	return nil
 }
